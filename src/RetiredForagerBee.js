3e7c121257e6c54457b27a86d7684cfe8b75a2ad
khchoi <khchoi0301@gamil.com>
2018-09-28 21:02:07 +0900

    complete 6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,7 +1,24 @@
 const ForagerBee = require('./ForagerBee')
 
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor() {
+    super()
+    this.age = 40
+    this.job = 'gamble'
+    this.canFly = false
+    this.color = 'grey'
+    // this.temp = forage
+  }
+  
+  gamble(treasure) {
+    this.treasureChest.push(treasure)
+  }
+  forage() {
+    return 'I am too old, let me play cards instead'
+  }
+  // gamble(treasure) {
+  //   this.treasureChest.push(treasure)
+  // }
 }
 
 module.exports = RetiredForagerBee
